956e6628e3308325678e1e956489e61bf237f82f
PRONOB BISWAS <80696443+pronobiswas@users.noreply.github.com>
2025-11-14 18:03:58 +0600

    update

--- pages/patient_education.php
+++ pages/patient_education.php
@@ -12,9 +12,25 @@ if ( ! defined( 'ABSPATH' ) ) {
 get_header();
 ?>
 
-<div>
-    <h1 style="padding:120px 0">this is A clean <b>Patient_Education_page</b> page </h1>
-</div>
+<section id="PatientEducation">
+    <div class="myContiner">
+        <div class="Patient_Education_wrapper">
+            <div class="selectOption">
+                <select name="exercise" id="exercise">
+                    <option value="Home exercise program">Home exercise program 1</option>
+                    <option value="Home exercise program">Home exercise program 2</option>
+                    <option value="Home exercise program">Home exercise program 3</option>
+                </select>
+            </div>
+            <div class="exercise_document_wrapper">
+                <div class="exercise_document_item">
+                    <img src="http://localhost/smohamed/wp-content/uploads/2025/11/image-5.png" alt="exercise_document_item">
+                </div>
+            </div>
+        </div>
+        <?php get_template_part('template-parts/FrequentQA', 'FrequentQA'); ?>
+    </div>
+</section>
 
 
 <?php
